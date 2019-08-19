01879bec7320330f377f42168899b045bee662de
Amit Das Gupta <amitdsgupta007@gmail.com>
2019-08-19 15:20:47 +0530

    Installed EJS and setup view engine

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const app=express();
 // use express router
 app.use('/',require('./routes'));
 
+app.set('view engine','ejs');
+app.set('views','./view');
+
 app.listen(port,function(err){
     if(err){
         console.log(`error ocuured: ${err}`);
