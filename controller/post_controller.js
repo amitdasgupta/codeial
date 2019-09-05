0ef943f62bb061633457ebada25207b25b1e7997
Amit Das Gupta <amitdsgupta007@gmail.com>
2019-09-05 19:50:41 +0530

    check authentication for creating post

--- controller/post_controller.js
+++ controller/post_controller.js
@@ -1,7 +1,5 @@
 const Post=require('../model/post');
 module.exports.create=function(req,res){
-    if(req.isAuthenticated())
-    {
         Post.create({
           content:req.body.content,
           user:req.user._id
@@ -11,8 +9,7 @@ module.exports.create=function(req,res){
                 console.log(`error in creating post:${err}`);
                 return;
             }
+            return res.redirect('back');
         });
-    }
-    return res.redirect('back');
 }
 
