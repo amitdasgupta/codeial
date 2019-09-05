cc65fd97cff2fbfb5105021b4576881e728feced
Amit Das Gupta <amitdsgupta007@gmail.com>
2019-09-05 18:21:59 +0530

    Prepopulate user to display author name

--- controller/home_controller.js
+++ controller/home_controller.js
@@ -3,12 +3,25 @@ const Post=require('../model/post');
 module.exports.home=function(req,res){
     // console.log(req.cookies);
     // res.cookie('user_id',25);
-    Post.find({},function(err,post){
+
+    // code to show post on homepage************
+    // Post.find({},function(err,post){
+    //     if(err)
+    //     console.log(`error in getting posts:${err}`);
+    //     return res.render('home',{
+    //         title:'Codeial | Home',
+    //         posts:post
+    //     });
+    // })
+    
+    // code to populate user attribute with user data from database
+    Post.find({}).populate('user').exec(function(err,post){
         if(err)
         console.log(`error in getting posts:${err}`);
         return res.render('home',{
             title:'Codeial | Home',
             posts:post
         });
-    })
+    });
+    
 };
