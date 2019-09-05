7cba02f0787cf87b01c885d74c1f0782a7581e33
Amit Das Gupta <amitdsgupta007@gmail.com>
2019-09-05 17:59:29 +0530

    Add posts in homepage

--- controller/home_controller.js
+++ controller/home_controller.js
@@ -1,7 +1,14 @@
+const Post=require('../model/post');
+
 module.exports.home=function(req,res){
-    console.log(req.cookies);
-    res.cookie('user_id',55);
-    return res.render('home',{
-        title:'Home'
-    });
+    // console.log(req.cookies);
+    // res.cookie('user_id',25);
+    Post.find({},function(err,post){
+        if(err)
+        console.log(`error in getting posts:${err}`);
+        return res.render('home',{
+            title:'Codeial | Home',
+            posts:post
+        });
+    })
 };
