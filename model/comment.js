5d458e2c9f9b2e4a1bf256729f351338552e8626
Amit Das Gupta <amitdsgupta007@gmail.com>
2019-09-06 09:11:12 +0530

    Add comment schema and join users schema with it

---
+++ model/comment.js
@@ -0,0 +1,23 @@
+const mongoose=require('mongoose');
+
+const commentSchema=new mongoose.Schema({
+    content: {
+        type: String,
+        required: true
+    },
+    // include the user who commented
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    // include the post on which comment made
+    post:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Post'
+    }
+},{
+    timestamps: true
+});
+
+const Comment=mongoose.model('Comment',commentSchema);
+module.exports=Comment;
--- model/post.js
+++ model/post.js
@@ -8,7 +8,14 @@ const postSchema=new mongoose.Schema({
     user:{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
-    }
+    },
+    // include array of ids all comments in post schema itself
+    comments: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'comment'
+        }
+    ]
 },{
     timestamps: true
 });
