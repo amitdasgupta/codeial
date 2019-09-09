75eecbfae2c897176fc5d2e4916960f777da9cfe
Amit Das Gupta <amitdsgupta007@gmail.com>
2019-09-09 12:55:24 +0530

    Added delete comment feature and update code so that post owner can also delete a comment

--- routes/comments.js
+++ routes/comments.js
@@ -1,7 +1,9 @@
 const express=require('express');
 const router=express.Router();
 const commentsController=require('../controller/comments_controller');
+const passport=require('passport');
 
 router.post('/create',commentsController.create);
+router.get('/destroy/:id',passport.checkAuthentication,commentsController.destroy);
 
 module.exports=router;
