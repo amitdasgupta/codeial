aecbf17b378e5024efe53726a95fcfc6459145d0
Amit Das Gupta <amitdsgupta007@gmail.com>
2019-09-19 15:08:48 +0530

    Implemented display posts and delete post api

--- controller/api/v1/posts_api.js
+++ controller/api/v1/posts_api.js
@@ -1,6 +1,41 @@
-module.exports.index=function(req,res){
-    return res.json(200,{
+const Post=require('../../../model/post');
+const Comment=require('../../../model/comment');
+
+module.exports.index=async function(req,res){
+    // here password is excluded
+    let post=await Post.find({})
+        .sort('-createdAt')
+        .populate('user','-password')
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user',
+                select: '-password'
+            },
+            options: { sort: { 'created_at': -1 } } 
+    });
+
+    return res.status(200).json({
         message: "List of posts",
-        posts: []
-    })
+        posts: post
+    });
+}
+
+module.exports.destroy=async function(req,res){
+    try{
+      let post=await Post.findById(req.params.id);
+      // .id converting the object id into string
+          post.remove();
+          await Comment.deleteMany({post: req.params.id});
+          
+          return res.json(200,{
+              message: "Post and associated comment deleted successfully"
+          })
+    }
+          catch(err){
+          console.log(err);
+          return res.json(500,{
+              message: "Internal server error"
+          });
+        }
 }
--- routes/api/v2/posts.js
+++ routes/api/v2/posts.js
@@ -1,9 +1,8 @@
 const express=require('express');
 
 const router=express.Router();
-const postApi=require('../../../controller/api/v1/posts_api');
+const postApi=require('../../../controller/api/v2/posts_api');
 
 router.get('/',postApi.index);
-router.delete('/:id',postApi.destroy);
 
 module.exports=router;
