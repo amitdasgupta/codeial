d21791303f53c3705d28dc7da75dfce9671e4f25
amitdasgupta <amitdsgupta007@gmail.com>
2019-12-07 20:04:34 +0530

    used redis-server and parallel processing

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -1,31 +1,36 @@
-const nodemailer=require("nodemailer");
-const ejs=require("ejs");
-const path=require("path");
+const nodemailer = require("nodemailer");
+const ejs = require("ejs");
+const path = require("path");
 
 let transporter = nodemailer.createTransport({
-    service: 'gmail',
-    host: 'smtp.gmail.com',
-    port: 587,
-    secure: false, // true for 465, false for other ports
-    auth: {
-        user: "", // generated ethereal user
-        pass: ""// generated ethereal password
-    }
+  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // true for 465, false for other ports
+  auth: {
+    user: "", // generated ethereal user
+    pass: "" // generated ethereal password
+  }
 });
 
-let renderTemplate=(data,relativePath)=>{
-    let mainHtml;
-    ejs.renderFile(path.join(__dirname,'../view/mailers',relativePath),
+let renderTemplate = (data, relativePath) => {
+  let mainHtml;
+  ejs.renderFile(
+    path.join(__dirname, "../view/mailers", relativePath),
     data,
-    function(err,template){
-        console.log(template);
-        if(err){console.log('error in displaying template',err);return;}
-        mainHtml=template;
-    });
-    return mainHtml;
-}
+    function(err, template) {
+      console.log(template);
+      if (err) {
+        console.log("error in displaying template", err);
+        return;
+      }
+      mainHtml = template;
+    }
+  );
+  return mainHtml;
+};
 
-module.exports={
-    transporter: transporter,
-    renderTemplate: renderTemplate
-}
+module.exports = {
+  transporter: transporter,
+  renderTemplate: renderTemplate
+};
--- controller/comments_controller.js
+++ controller/comments_controller.js
@@ -1,58 +1,67 @@
-const Comment=require('../model/comment');
-const Post=require('../model/post');
-const commentMailer=require('../mailers/comment_mailer');
-module.exports.create=async function(req,res){
+const Comment = require("../model/comment");
+const Post = require("../model/post");
+const commentEmailWorker = require("../workers/comment_email_worker");
+const queue = require("../config/kue");
 
-    try{
-        let post= await Post.findById(req.body.post);
-        let comment=await Comment.create({
-            content: req.body.content,
-            user: req.user._id,
-            post: req.body.post 
-            });
-        post.comments.push(comment);
-        post.save();
-        let commentUser= await Comment.findById(comment._id).populate('user','-password');
-        commentMailer.newComment(commentUser);
-        if(req.xhr){
-            return res.status(200).json({
-                data: commentUser,
-                message: "Comment created"
-            });
-        }
-        return res.redirect('back');
-    }
-    catch(err){
-        req.flash('error',err); 
-        return res.redirect('back');
-    }
-}
+module.exports.create = async function(req, res) {
+  try {
+    let post = await Post.findById(req.body.post);
+    let comment = await Comment.create({
+      content: req.body.content,
+      user: req.user._id,
+      post: req.body.post
+    });
+    post.comments.push(comment);
+    post.save();
+    let commentUser = await Comment.findById(comment._id).populate(
+      "user",
+      "-password"
+    );
 
-module.exports.destroy=async function(req,res){
-    try{
-        let comment=await Comment.findById(req.params.id).populate('post');
-        if(comment.user==req.user.id||req.user.id==comment.post.user){
-            let postId=comment.post;
-            comment.remove();
-        //    find post and then pull out that comment id from comments
-            let post=await Post.findByIdAndUpdate(postId,{ $pull: {comments: req.params.id}});
-            if(req.xhr){
-                return res.status(200).json({
-                    data:{
-                        comment_id: req.params.id
-                    },
-                    message: "Comment deleted"
-                });
-            }
-            return res.redirect('back');
-        }
-        else{
-            req.flash('error','You can not delete comment');
-            return res.redirect('back');
-        }
+    let job = queue.create("emails", commentUser).save(function(err) {
+      if (err) {
+        console.log("error in creating queue");
+      }
+      console.log("job enqueud", job.id);
+    });
+    if (req.xhr) {
+      return res.status(200).json({
+        data: commentUser,
+        message: "Comment created"
+      });
     }
-    catch(err){
-        req.flash('error',err); 
-        return res.redirect('back');
+    return res.redirect("back");
+  } catch (err) {
+    req.flash("error", err);
+    return res.redirect("back");
+  }
+};
+
+module.exports.destroy = async function(req, res) {
+  try {
+    let comment = await Comment.findById(req.params.id).populate("post");
+    if (comment.user == req.user.id || req.user.id == comment.post.user) {
+      let postId = comment.post;
+      comment.remove();
+      //    find post and then pull out that comment id from comments
+      let post = await Post.findByIdAndUpdate(postId, {
+        $pull: { comments: req.params.id }
+      });
+      if (req.xhr) {
+        return res.status(200).json({
+          data: {
+            comment_id: req.params.id
+          },
+          message: "Comment deleted"
+        });
+      }
+      return res.redirect("back");
+    } else {
+      req.flash("error", "You can not delete comment");
+      return res.redirect("back");
     }
-}
+  } catch (err) {
+    req.flash("error", err);
+    return res.redirect("back");
+  }
+};
