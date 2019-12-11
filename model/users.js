e499a6a645db7efe53a9d03278ff196267799e08
amitdasgupta <amitdsgupta007@gmail.com>
2019-12-11 09:32:59 +0530

    Added friendship schema

--- model/users.js
+++ model/users.js
@@ -1,48 +1,56 @@
-const mongoose=require('mongoose');
+const mongoose = require("mongoose");
 
-const multer=require('multer');
-const path=require('path');
-const AVATAR_PATH=path.join('/uploads/users/avatars'); 
+const multer = require("multer");
+const path = require("path");
+const AVATAR_PATH = path.join("/uploads/users/avatars");
 
-const userSchema=new mongoose.Schema({
-   email:{
-       type:String,
-       required:true,
-       unique:true
-   },
-   password:{
-       type:String,
-       required:true
-   },
-   name:{
-       type:String,
-       required:true
-   },
-   avatar:{
-       type: String
-   }
-},{
-    timestamps:true
-});
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    name: {
+      type: String,
+      required: true
+    },
+    avatar: {
+      type: String
+    },
+    friendships: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Friendship"
+      }
+    ]
+  },
+  {
+    timestamps: true
+  }
+);
 
 // telling mongoose that it is a model
 
 let storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, path.join(__dirname,'..',AVATAR_PATH));
-    },
-    filename: function (req, file, cb) {
-      cb(null, file.fieldname + '-' + Date.now())
-    }
-  });
-
+  destination: function(req, file, cb) {
+    cb(null, path.join(__dirname, "..", AVATAR_PATH));
+  },
+  filename: function(req, file, cb) {
+    cb(null, file.fieldname + "-" + Date.now());
+  }
+});
 
 // static methods
-userSchema.statics.uploadedAvatar=multer({storage: storage}).single('avatar');
-userSchema.statics.avatarPath=AVATAR_PATH;
-   
+userSchema.statics.uploadedAvatar = multer({ storage: storage }).single(
+  "avatar"
+);
+userSchema.statics.avatarPath = AVATAR_PATH;
 
+const User = mongoose.model("User", userSchema);
 
-const User=mongoose.model('User',userSchema);
-
-module.exports=User;
+module.exports = User;
